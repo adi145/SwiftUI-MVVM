2460b13e7864a24ece1cd40072146094f98eaeeb
Adinarayana Machavarapu <adinarayana.m@cimbthai.com>
2020-06-26 10:01:53 +0700

    Initial Commit

---
+++ SampleSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleSwiftUIDemo
+//
+//  Created by Adinarayana Machavarapu on 26/6/2563 BE.
+//  Copyright © 2563 Adinarayana Machavarapu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
